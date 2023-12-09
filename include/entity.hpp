304776c3b729b6525b8ceb8d35f356003f73b601
theo Villemer <theo.villemer@epitech.eu>
2023-12-09 03:05:49 +0100

    [ADD] basic push

---
+++ include/entity.hpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2023
+** game_jam_epitech_09_12_23
+** File description:
+** entity.hpp
+*/
+
+#pragma once
+
+#include "raylib.h"
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** game_jam_epitech_09_12_23
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+#include "entity.hpp"
+
+int main(int ac, char **av) {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
