b9eb9ad22a7846d73e63a3d2868138d80cab44fb
laetitia <laetitia.bousch@epitech.eu>
2023-12-10 11:37:59 +0100

    feat: tetris game

--- include/Tetris.hpp
+++ include/Tetris.hpp
@@ -9,12 +9,60 @@
 
 #include "display.hpp"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define SQUARE_SIZE             20
+
+#define GRID_HORIZONTAL_SIZE    12
+#define GRID_VERTICAL_SIZE      20
+
+#define LATERAL_SPEED           10
+#define TURNING_SPEED           12
+#define FAST_FALL_AWAIT_COUNTER 30
+
+#define FADING_TIME             33
+
+typedef enum GridSquare { EMPTY, MOVING, FULL, BLOCK, FADING } GridSquare;
+
 class Tetris {
     public:
-        void init() {
-            return;
-        };
-        void run() {
-            return;
-        };
+        void init();
+        void run();
+        void UpdateGame();
+        void DrawGame();
+        bool Createpiece();
+        void GetRandompiece();
+        void ResolveFallingMovement(bool *detection, bool *pieceActive);
+        bool ResolveLateralMovement();
+        bool ResolveTurnMovement();
+        void CheckDetection(bool *detection);
+        void CheckCompletion(bool *lineToDelete);
+        int DeleteCompleteLines();
+
+    private:
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+        bool gameOver = false;
+        bool pause = false;
+        GridSquare grid [GRID_HORIZONTAL_SIZE][GRID_VERTICAL_SIZE];
+        GridSquare piece [4][4];
+        GridSquare incomingPiece [4][4];
+        int piecePositionX = 0;
+        int piecePositionY = 0;
+        Color fadingColor;
+        bool beginPlay = true;
+        bool pieceActive = false;
+        bool detection = false;
+        bool lineToDelete = false;
+        int level = 1;
+        int lines = 0;
+        int gravityMovementCounter = 0;
+        int lateralMovementCounter = 0;
+        int turnMovementCounter = 0;
+        int fastFallMovementCounter = 0;
+        int fadeLineCounter = 0;
+        int gravitySpeed = 30;
 };
